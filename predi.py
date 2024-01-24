805e2945a0938ee0de1590bb7428acfb541f8d3c
LeDiabetique <diothugo36@gmail.com>
2024-01-24 11:09:52 +0100

    Commented code

--- predi.py
+++ predi.py
@@ -1,61 +1,94 @@
 import pandas as pd
 import numpy as np
 
-try:
-    data = pd.read_csv('./data.csv')
-    if data.empty:
-        print("File data.csv is empty")
+def load_csv(csv_file):
+    """
+    This function load a .csv file and return a pandas dataframe
+    args: csv_file
+    csv_file: path of the .csv file
+    return: Datas of the .csv file
+    """
+    try:
+        data = pd.read_csv(csv_file)
+        if data.empty:
+            print("File .csv is empty")
+            exit()
+        data.dropna(inplace=True) # Remove incomplete values
+        return data
+    except FileNotFoundError:
+        print("File .csv not found")
         exit()
-    data.dropna(inplace=True) # Remove incomplete values
-except FileNotFoundError:
-    print("File data.csv not found")
-    exit()
 
-try:
-    theta_data = pd.read_csv('./theta.csv')
-    if theta_data.empty:
+def predict(x, theta):
+    """
+    Simple fonction to calculate the price of a car with a mileage in km
+    args: x, theta
+    x: mileage in km
+    theta: thetas value
+    return: Price of the car
+    """
+    return x.dot(theta)
+
+def predict_result():
+    """
+    This function predict the price of a car with a mileage in km
+    The result depends on the thetas value.
+    If you train the model before using it you will have a better result.
+    """
+    data = load_csv('./data.csv')
+    try:
+        theta_data = pd.read_csv('./theta.csv')
+        if theta_data.empty:
+            km_mean = 1
+            km_std = 1
+            theta0 = 0
+            theta1 = 0
+        else:
+            theta0 = theta_data['theta0'][0]
+            theta1 = theta_data['theta1'][0]
+            km_std = theta_data['km_std'][0]
+            km_mean = theta_data['km_mean'][0]
+    except FileNotFoundError:
         km_mean = 1
         km_std = 1
         theta0 = 0
         theta1 = 0
-    else:
-        theta0 = theta_data['theta0'][0]
-        theta1 = theta_data['theta1'][0]
-        km_std = theta_data['km_std'][0]
-        km_mean = theta_data['km_mean'][0]
-except FileNotFoundError:
-    km_mean = 1
-    km_std = 1
-    theta0 = 0
-    theta1 = 0
-# print(theta)
-theta = np.zeros((2,1))
-theta[0] = theta0
-theta[1] = theta1
 
-def predict(x, theta):
-    return x.dot(theta)
+    theta = np.zeros((2,1))
+    theta[0] = theta0
+    theta[1] = theta1
 
-print("Want to know a car price ?")
-print("Enter a mileage in km (only numbers) or write EXIT to leave")
-while True:
-    try:
-        km_input = input("Kilometers : ")
-        if km_input == "EXIT":
+    print("Want to know a car price ?")
+    print("Enter a mileage in km (only numbers) or write EXIT to leave")
+    while True:
+        try:
+            km_input = input("Kilometers : ")
+            if km_input == "EXIT":
+                break
+            if km_input.isdigit():
+                km_input = float(km_input)
+                km_input = np.array([[km_input, 1]])
+                km_input[:, 0] = (km_input[:, 0] - km_mean) / km_std
+                
+                result = predict(km_input, theta)
+                if result[0, 0] < 0:
+                    result[0, 0] = 0
+                print("The car may cost {:.0f} €".format(result[0, 0]))
+
+            else:
+                print("Please enter a mileage in km (only numbers) or write EXIT to leave ")
+        except EOFError:
+            print("\nBye")
             break
-        if km_input.isdigit():
-            km_input = float(km_input)
-            km_input = np.array([[km_input, 1]])  # Créez un tableau NumPy avec la structure correcte
-            km_input[:, 0] = (km_input[:, 0] - km_mean) / km_std
-            
-            result = predict(km_input, theta)
-            print("The car may cost {:.0f} €".format(result[0, 0]))
+        except KeyboardInterrupt:
+            print("\nBye")
+            break
 
-        else:
-            print("Please enter a mileage in km (only numbers) or write EXIT to leave ")
-    except EOFError:
-        print("\nBye")
-        break
-    except KeyboardInterrupt:
-        print("\nBye")
-        break
+def main():
+    """
+    This program predict the price of a car with a mileage in km
+    """
+    predict_result()
+
+if __name__ == "__main__":
+    main()
--- training.py
+++ training.py
@@ -3,38 +3,42 @@ import numpy as np
 import matplotlib
 matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
+from predi import load_csv
 
 
-try:
-    data = pd.read_csv('./data.csv')
-    if data.empty:
-        print("File data.csv is empty")
-        exit()
-    data.dropna(inplace=True) # Remove incomplete values
-except FileNotFoundError:
-    print("File data.csv not found")
-    exit()
-
-learning_rate = 0.01
-learning_loop = 1000
-
-X =  data['km'].values
-X = np.c_[X, np.ones(X.shape[0])]
-Y = data['price'].values
-Y = Y.reshape(-1,1)
-theta = np.zeros((2,1))
-m = data.shape[0]
-x_mean = X[:,0].mean()
-std_x = X[:,0].std()
-X[:,0] = (X[:,0] - x_mean) / std_x
 
 def model(X, theta):
+    """
+    Simple fonction to calculate the price of a car with a mileage in km
+    args: X, theta
+    X: mileage in km
+    theta: thetas value
+    return: Price of the car
+    """
     return X.dot(theta)
 
 def grad(X, Y, theta):
-    return 1 / m * X.T.dot(model(X, theta) - Y)
+    """
+    This function calculate the gradient 
+    args: X, Y, theta
+    X: mileage in km
+    Y: price of the car
+    theta: thetas value
+    return: Gradient to update the thetas
+    """
+    return 1 / X.shape[0] * X.T.dot(model(X, theta) - Y)
 
 def gradient_descent(X, Y, theta, learning_rate, learning_loop):
+    """
+    This function calculate the gradient descent
+    args: X, Y, theta, learning_rate, learning_loop
+    X: mileage in km
+    Y: price of the car
+    theta: thetas value
+    learning_rate: learning rate of the gradient descent
+    learning_loop: number of iterations
+    return: thetas before training and cost history to be able to plot the cost function
+    """
     cost_history = np.zeros(learning_loop)
     for loop in range(learning_loop):
         theta = theta - learning_rate * grad(X, Y, theta)
@@ -42,9 +46,24 @@ def gradient_descent(X, Y, theta, learning_rate, learning_loop):
     return theta, cost_history
 
 def cost_function(X, Y, theta):
-    return 1 / (2 * m) * np.sum((model(X, theta) - Y) ** 2)
+    """
+    This function calculate the cost function
+    args: X, Y, theta
+    X: mileage in km
+    Y: price of the car
+    theta: thetas value
+    return: Cost determined by the thetas to fill a cost history
+    """
+    return 1 / (2 * X.shape[0]) * np.sum((model(X, theta) - Y) ** 2)
 
-def algo_perf(prediction):
+def algo_perf(prediction, Y):
+    """
+    This function calculate the coefficient of determination R² to evaluate the performance of the algorithm
+    args: prediction, Y
+    prediction: prediction of the price of the car
+    Y: price of the car
+    return: Coefficient of determination R²
+    """
     mean_Y = np.mean(Y)
     SST = np.sum((Y - mean_Y) ** 2)
     SSR = np.sum((Y - prediction) ** 2)
@@ -52,28 +71,54 @@ def algo_perf(prediction):
     print("Coefficient de détermination R² :", R_sq)
 
 
-theta, cost_history = gradient_descent(X, Y, theta, learning_rate, learning_loop)
-datacsv = {'theta0': [theta[0,0]], 'theta1': [theta[1,0]], 'km_std': [std_x], 'km_mean': [x_mean]}
-theta_data = pd.DataFrame(datacsv, columns=['theta0', 'theta1', 'km_std', 'km_mean'])
-theta_data.to_csv('./theta.csv', index=False)
-predict = model(X, theta)
-algo_perf(predict)
-# Print the 2 plot in the same time
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 5))
-ax1.plot(range(learning_loop), cost_history, color='blue')
-ax1.set_title('Cost function', color='blue')
-ax1.set_xlabel('Number of iterations')
-ax1.set_ylabel('Cost')
+def print_stats(data, cost_history, predict, learning_loop):
+    """
+    This function plot the cost function and the scatter distribution of price vs km
+    args: data, cost_history, predict, learning_loop
+    data: data from the csv file
+    cost_history: cost history to plot the cost function
+    predict: prediction of the price of the car
+    learning_loop: number of iterations
+    """
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 5))
+    ax1.plot(range(learning_loop), cost_history, color='blue')
+    ax1.set_title('Cost function', color='blue')
+    ax1.set_xlabel('Number of iterations')
+    ax1.set_ylabel('Cost')
 
-# Deuxième graphique (Regression line)
-ax2.scatter(data['km'], data['price'], color='blue', s=10)
-ax2.plot(data['km'], predict, color='red')
-ax2.set_xlabel('Mileage (Km)', color='blue')
-ax2.set_ylabel('Price (€)', color='blue')
-ax2.set_title('Scatter distribution of price vs km', color='blue')
-ax2.xaxis.set_major_formatter(plt.FormatStrFormatter('%d'))
-ax2.xaxis.set_major_locator(plt.MultipleLocator(40000))
-
-if __name__ == "__main__":
+    ax2.scatter(data['km'], data['price'], color='blue', s=10)
+    ax2.plot(data['km'], predict, color='red')
+    ax2.set_xlabel('Mileage (Km)', color='blue')
+    ax2.set_ylabel('Price (€)', color='blue')
+    ax2.set_title('Scatter distribution of price vs km', color='blue')
+    ax2.xaxis.set_major_formatter(plt.FormatStrFormatter('%d'))
+    ax2.xaxis.set_major_locator(plt.MultipleLocator(40000))
     plt.tight_layout()
     plt.show()
+
+def main():
+    """
+    Program to train the model with a linear regression
+    """
+    data = load_csv('./data.csv')
+    learning_rate = 0.01
+    learning_loop = 1000
+    X =  data['km'].values
+    X = np.c_[X, np.ones(X.shape[0])]
+    Y = data['price'].values
+    Y = Y.reshape(-1,1)
+    theta = np.zeros((2,1))
+    x_mean = X[:,0].mean()
+    std_x = X[:,0].std()
+    X[:,0] = (X[:,0] - x_mean) / std_x
+    theta, cost_history = gradient_descent(X, Y, theta, learning_rate, learning_loop)
+    datacsv = {'theta0': [theta[0,0]], 'theta1': [theta[1,0]], 'km_std': [std_x], 'km_mean': [x_mean]}
+    theta_data = pd.DataFrame(datacsv, columns=['theta0', 'theta1', 'km_std', 'km_mean'])
+    theta_data.to_csv('./theta.csv', index=False)
+    predict = model(X, theta)
+    algo_perf(predict, Y)
+    print_stats(data, cost_history, predict, learning_loop)
+
+
+if __name__ == "__main__":
+    main()
